c9dc7f8c83c95b319deac20c09b455e28445d7c9
simakasare123 <97290562+simakasare123@users.noreply.github.com>
2025-09-10 15:24:47 +0530

    changes

---
+++ src/lib/modals/design.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+
+export interface DesignEntry extends Document {
+  containt: string;
+  heading: string;
+  subHead1?: string;
+  subHead2?: string;
+  image: string;
+}
+
+
+const DesignSchema = new Schema<DesignEntry>(
+  {
+    containt: { type: String, required: true },
+    heading: { type: String, required: true },
+    subHead1: { type: String },
+    subHead2: { type: String },
+    image : String
+  },
+  { timestamps: true, collection: "design" } 
+);
+
+
+export const DesignModel: Model<DesignEntry> =
+  mongoose.models.Design || mongoose.model<DesignEntry>("Design", DesignSchema);

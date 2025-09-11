9f25ce7ac3222f2d0095a9b5db08a9ae8a4373d9
pratim2003 <duttapratim003@gmail.com>
2025-09-11 15:52:26 +0530

    contactus

---
+++ src/app/api/fpii/contactus/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest } from "next/server";
+import connect from "src/lib/db";
+import contactusModel from "src/lib/modals/contactUs";
+
+export async function POST(req:NextRequest){
+    await connect()
+    try {
+        const data = await req.json()
+        await contactusModel.create(...data)
+        return Response.json({
+            success :true,
+            message : "message created"
+        })
+    } catch (error) {
+        console.error("Error fetching designs:", error);
+    return Response.json(
+      {
+        message: "Error getting designs",
+        error: error.message || error,
+      },
+      { status: 500 }
+    );
+    }
+}

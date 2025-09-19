ea02ee3a5853ec6e0634766d28b7e6cb7ec3b576
pratim2003 <duttapratim003@gmail.com>
2025-09-19 11:43:11 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
-// 
+// // 
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//   trailingSlash: true,
+//   env: {
+//     BUILD_STATIC_EXPORT: 'true', // server mode (keeps API routes working)
+//   },
+//     images: {
+//     unoptimized: true,
+//   },
+//   output:'export',
+//   modularizeImports: {
+//     '@mui/icons-material': {
+//       transform: '@mui/icons-material/{{member}}',
+//     },
+//     '@mui/material': {
+//       transform: '@mui/material/{{member}}',
+//     },
+//     '@mui/lab': {
+//       transform: '@mui/lab/{{member}}',
+//     },
+//   },
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     });
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
+
 
 /**
  * @type {import('next').NextConfig}
@@ -6,12 +41,11 @@
 const nextConfig = {
   trailingSlash: true,
   env: {
-    BUILD_STATIC_EXPORT: 'true', // server mode (keeps API routes working)
+    BUILD_STATIC_EXPORT: 'false', // or remove this if not needed
   },
-    images: {
+  images: {
     unoptimized: true,
   },
-  output:'export',
   modularizeImports: {
     '@mui/icons-material': {
       transform: '@mui/icons-material/{{member}}',
@@ -31,5 +65,6 @@ const nextConfig = {
     return config;
   },
 };
+
 
 export default nextConfig;
